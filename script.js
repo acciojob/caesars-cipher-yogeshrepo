9368bbfa59b00886f7ab458d32e11fdacae4b939
yogeshrepo <118822006+yogeshrepo@users.noreply.github.com>
2023-06-12 11:46:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ function rot13(Str) {
 	  else decodedArr.push(lookup[Str[i]])
   }
 
-  return; //return decodedArr
+  return decodedArr; //return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
